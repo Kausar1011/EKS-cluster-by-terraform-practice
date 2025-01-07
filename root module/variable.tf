f029806d894b1334899192422d6ee69e60b9c009
teju2707 <tejaswiniprasadwakte@gmail.com>
2025-01-07 13:35:41 +0530

    Update variable.tf

--- root module/variable.tf
+++ root module/variable.tf
@@ -16,10 +16,8 @@ variable "cluster_name" {
   default     = "my-eks-cluster"
 }
 
-
-
 variable "subnet_cidr_blocks" {
   description = "List of CIDR blocks for the subnets"
   type        = list(string)
-  default = ["10.0.1.0/24" , "10.0.2.0/24"]
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
