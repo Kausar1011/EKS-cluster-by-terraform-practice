1570bb33d1382231deb5cb0a4c1ca3b861b26acc
teju2707 <tejaswiniprasadwakte@gmail.com>
2025-01-07 13:05:49 +0530

    Update variable.tf

--- module/variable.tf
+++ module/variable.tf
@@ -1,20 +1,22 @@
 variable "aws_region" {
-  description = "AWS region for the resources"
-  default  = us-east-1
+  description = "The AWS region"
   type        = string
+  default     = "us-east-1"
 }
 
 variable "vpc_cidr_block" {
-  description = "CIDR block for the VPC"
-  default = 10.0.0.0/16
+  description = "VPC CIDR block"
   type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "cluster_name" {
-  description = "Name of the EKS cluster"
-    default  = us-east-1
+  description = "EKS Cluster Name"
   type        = string
+  default     = "my-eks-cluster"
 }
+
+
 
 variable "subnet_cidr_blocks" {
   description = "List of CIDR blocks for the subnets"
